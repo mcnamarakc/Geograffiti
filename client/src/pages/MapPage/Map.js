82f52982a2553ba82247177b62f205f9d8adef73
Lara Eller <lareh_eller@yahoo.com>
2019-06-04 13:17:51 -0400

    map work

--- client/src/pages/MapPage/Map.js
+++ client/src/pages/MapPage/Map.js
@@ -29,7 +29,9 @@ renderNodaMarkers = event => {
   API.ArtPage.getNeighborhood("NODA")
   .then(res => {
     this.setState({
-      markers: res.data.map(item => [item.latitude, item.longitude])
+      markers: res.data.map(item => {
+        return {position:[item.latitude, item.longitude]}
+      })
     })
   })
   .catch(err => console.log(err));
@@ -46,11 +48,11 @@ renderNodaMarkers = event => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {/* <Marker position={this.state.markers.map(item => {item})}>
+        <Marker markers={this.state.markers} >
           <Popup>
             A pretty CSS3 popup. <br/> Easily customizable.
           </Popup>
-        </Marker> */}
+        </Marker>
       </LeafletMap>
       <button onClick={this.renderNodaMarkers} type="button" className="btn btn-secondary">Noda</button>
       <button onClick={this.renderMidwoodMarkers} type="button" className="btn btn-secondary">Plaza Midwood</button>
