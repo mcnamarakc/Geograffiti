5406c31013502f4006f41e3fae4e004be69d57c0
Paul Whurr <pawhurr@gmail.com>
2019-06-09 12:12:56 -0400

    about page typo fixes, mobile fix for insta links for artists in bio

--- client/src/components/App/InstaList.js
+++ client/src/components/App/InstaList.js
@@ -7,19 +7,19 @@ function InstaList() {
         <Center>
             <div className="instaContainer">"
                 <ul className="list-group flex-md-row">
-                    <li className="list-group-item">
+                    <li className="list-group-item m-1">
                         <a href="https://www.instagram.com/hookermedia/"><button className="instaBtn" onClick="window.location.href='https://www.instagram.com/hookermedia/';"><i className="fab fa-instagram"></i></button></a>
                         <p className="instaName">Matt Hooker</p>
                     </li>
-                    <li className="list-group-item">
+                    <li className="list-group-item m-1">
                         <a href="https://www.instagram.com/puckmcgruff/"><button className="instaBtn" onClick="window.location.href='https://www.instagram.com/puckmcgruff/';"><i className="fab fa-instagram"></i></button></a>
                         <p className="instaName">Matt Moore</p>
                     </li>
-                    <li className="list-group-item">
+                    <li className="list-group-item m-1">
                         <a href="https://www.instagram.com/mr_puckett/"><button className="instaBtn" onClick="window.location.href='https://www.instagram.com/mr_puckett/';"><i className="fab fa-instagram"></i></button></a>
                         <p className="instaName">William Puckett</p>
                     </li>
-                    <li className="list-group-item">
+                    <li className="list-group-item m-1">
                         <a href="https://www.instagram.com/napoletanoart/"><button className="instaBtn" onClick="window.location.href='https://www.instagram.com/napoletanoart/';"><i className="fab fa-instagram"></i></button></a>
                         <p className="instaName">Nick Napoletano</p>
                     </li>
